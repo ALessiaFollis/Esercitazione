1110f718524f280d73cb7c2fabed473d658add9b
Alessia <follisalessia@gmail.com>
2023-01-11 10:50:06 +0100

    esCalcolatrice

--- calcolatrice/src/test/java/it/alessiaf/corso/CalcolatriceTest.java
+++ calcolatrice/src/test/java/it/alessiaf/corso/CalcolatriceTest.java
@@ -1,9 +1,18 @@
 package it.alessiaf.corso;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 public class CalcolatriceTest {
+	
+	@Test
+	public void checkCostruttore() {
+		Calcolatrice c = new Calcolatrice();
+		assertSame(Calcolatrice.class, c.getClass());
+	}
 
 	@Test
 	public void checkOperazioni() {
@@ -16,5 +25,14 @@ public class CalcolatriceTest {
 		assertEquals(cal.Divisione(a, b), a/b);
 		assertEquals(cal.Potenza(a, b), Math.pow(a, b));
 		assertEquals(cal.Media(a, b), (a+b)/2);
+	}
+	
+	@Test
+	public void checkZero() {
+		Calcolatrice calc = new Calcolatrice();
+		IllegalArgumentException illegalArgumentException =
+				 assertThrows(IllegalArgumentException.class,
+	                		() -> calc.Divisione(1, 0));
+		 assertEquals("Non puoi dividere per zero!",illegalArgumentException.getMessage());
 	}
 }
